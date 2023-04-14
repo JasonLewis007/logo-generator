8a8a802c46216b4e02158fb6000af47e9aac7e2c
Jason Lewis <pensfan5050@gmail.com>
2023-04-14 18:17:55 -0400

    7th commit-created code for square and triangle shapes

---
+++ Shapes/triangle.js
@@ -0,0 +1,13 @@
+
+const Shape = require("shapes");
+
+class Triangle extends Shape {
+    constructor(answers) {
+        super(answers);
+    }
+    render() {
+        return `<polygon points="100 0, 0, 0 50, 100" fill="${this.shapeColor}"/>`
+    }
+}
+
+module.exports = Triangle;
